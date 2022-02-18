49b07ab79ed9bda49b618bea42053df55a50edf4
Jared Galloway <jaredgalloway07@gmail.com>
2022-02-18 12:39:55 -0800

    latest

--- nextflow/sandbox/gcreplay-tools-test/run_gctree.sh
+++ nextflow/sandbox/gcreplay-tools-test/run_gctree.sh
@@ -7,9 +7,9 @@
 # THIS IS IF GC != 0
 # The other case will 
 
-GCDF=gc-df.csv
-GC_NUM_HC=19
-QUERY="GC_num_HC==${GC_NUM_HC}"
+GCDF=gc-df.csv                  # Full PR cell HK database (csv)
+GC_NUM_HC=19                    # GC you would like to extract
+QUERY="GC_num_HC==${GC_NUM_HC}" # The query on the dataset before fasta extraction
 
 # query the gc dataframe
 python gcreplay-tools.py query-df -df $GCDF -q $QUERY -o gc_${GC_NUM_HC}_hk.csv
@@ -17,6 +17,11 @@ echo done with query
 
 # convert some columns into headers and sequences for a basic fasta format conversion
 python gcreplay-tools.py gc-df-to-fasta --gc-df gc_${GC_NUM_HC}_hk.csv -o gc_${GC_NUM_HC}_hk.fasta
+
+
+# -------
+# Now running gctree
+# note this is not including the HDAG stuff yet
 
 #MUTFILES_1=../../data/mutability-files/
 #MUTFILES_2=../../data/mutability-files/
@@ -35,6 +40,9 @@ python gcreplay-tools.py gc-df-to-fasta --gc-df gc_${GC_NUM_HC}_hk.csv -o gc_${G
 #cd gc[N]HK
 
 # TODO describe
+# First we deduplicate the sequences and convert to phylip alignment format, 
+# and also determine sequence abundances. The deduplicate command writes 
+# the phylip alignment of unique sequences to stdout (which we redirect to a file here). 
 deduplicate gc_${GC_NUM_HC}_hk.fasta \
     --root naive \
     --idmapfile idmap-gc${GC_NUM_HC}-HK.txt \ # HERE What is the idmap. ID_HK -> ?
