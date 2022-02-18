383474bf545215c40335e42650f00f58e3cdff8d
Jared Galloway <jaredgalloway07@gmail.com>
2022-02-18 14:20:44 -0800

    ready for wrangle automation update container

--- nextflow/sandbox/gcreplay-tools-test/run_anno_wrangle.sh
+++ nextflow/sandbox/gcreplay-tools-test/run_anno_wrangle.sh
@@ -7,11 +7,11 @@ set -eu
 
 # 
 
-IGH_AIRR=2022-02-09/partis_annotation/PR-1-6/engrd/single-chain/partition-igh.tsv
-IGK_AIRR=2022-02-09/partis_annotation/PR-1-6/engrd/single-chain/partition-igk.tsv
+IGH_AIRR=PR-1-6/engrd/single-chain/partition-igh.tsv
+IGK_AIRR=PR-1-6/engrd/single-chain/partition-igk.tsv
 
 # TODO pass this down in the pipeline
-FASTA=2022-02-09/ranked_bcr_sequences_per_well/PR-1-6.fasta
+FASTA=PR-1-6.fasta
 
 # TODO change the key that's getting passed down with 
 # with the pipeline currently to the key_file?
