1928d74206de0137d88a0efa9778e82c02258ff2
jgallowa07 <jaredgalloway07@gmail.com>
2022-03-28 21:22:12 -0700

    new run BCR=5

--- nextflow/run_rhino.sh
+++ nextflow/run_rhino.sh
@@ -10,6 +10,7 @@ export PATH=$SINGULARITYROOT/bin/:$PATH
 nextflow run main.nf \
         --manifest "data/_ignore/miseq/manifest.csv" \
         --results "results/$(date -I)" \
+        --bcr_count_thresh 5 \
         -work-dir /fh/scratch/delete30/matsen_e/jgallowa/work/ \
         -profile fred_hutch_rhino \
         -with-dag "results/$(date -I)/dag.svg" \
