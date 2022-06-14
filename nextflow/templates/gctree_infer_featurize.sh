61f675e6f9fb34c41f34c5553543e40d539f8d06
jgallowa07 <jaredgalloway07@gmail.com>
2022-06-14 07:37:09 -0700

    update the dev to merge with replay tools

--- nextflow/templates/gctree_infer_featurize.sh
+++ nextflow/templates/gctree_infer_featurize.sh
@@ -69,7 +69,7 @@ export MPLBACKEND=Agg
 
 
 # ADD DMS DATA TO OBSERVED BCR SEQS
-gcreplay-tools-dev.py featurize-seqs \
+gcreplay-tools.py featurize-seqs \
     $GCDF \
     --variant_scores ${DMS_VSCORES} \
     --multi_variant_scores ${DMS_MULTI_SCORES} \
@@ -97,14 +97,14 @@ then
 # will not need to do this in the pipeline 
 
 # convert some columns into headers and sequences for a basic fasta format conversion
-gcreplay-tools-dev.py gc-df-to-fasta \
+gcreplay-tools.py gc-df-to-fasta \
     --gc-df $GCDF \
     -o $GC_DEF.fasta
 echo \(LOG\) done: created fasta
 
 
 # make isotype map for the specific gc
-gcreplay-tools-dev.py get-columns \
+gcreplay-tools.py get-columns \
     -df $GCDF \
     -c ID_HK -c isotype_HC \
     -o $GC_DEF.isotypemap
@@ -153,7 +153,7 @@ echo \(LOG\) done: gctree
 
 # Run will's featurize code
 #mkdir -p ${GC_DEF}-featurize-output/     # featurized rank 1 trees? could cobine with below 
-xvfb-run -a gcreplay-tools-dev.py featurize-nodes \
+xvfb-run -a gcreplay-tools.py featurize-nodes \
     ${OUTDIR}/gctree.inference.1.p \
     ${GC_DEF}.idmap \
     ${DMS_VSCORES} \
