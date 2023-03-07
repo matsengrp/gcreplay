88fba8c9eeff1b6a97c16fe249da899afba737b6
duncan ralph <dkralph@gmail.com>
2023-03-07 14:18:59 -0800

    work on plotting

--- analysis/run-gcdyn.sh
+++ analysis/run-gcdyn.sh
@@ -6,6 +6,6 @@ odir=/fh/fast/matsen_e/dralph/gcdyn/$vsn
 gcrdir=projects/gcreplay/analysis
 gcddir=projects/gcdyn
 
-echo ./$gcrdir/gcdyn-plot.py --data-dir $ddir --simu-dir $odir/simu --outdir $odir/plots
-# echo conda activate gcdyn
-# echo python $gcddir/scripts/multi-simulation.py --outdir $odir/simu --n-seqs 70 --n-trials 1
+echo ./$gcrdir/gcdyn-plot.py --data-dir $ddir --simu-dir $odir/simu --outdir $odir
+echo conda activate gcdyn
+echo python $gcddir/scripts/multi-simulation.py --outdir $odir/simu --n-seqs 70 --n-trials 1
