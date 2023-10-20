887ffe597ee959e8726ae535d9791128c07f6082
Erick Matsen <ematsen@gmail.com>
2023-10-20 13:54:22 -0700

    passenger-8a

--- analysis/passenger-blast/run-blast.sh
+++ analysis/passenger-blast/run-blast.sh
@@ -1,8 +1,5 @@
-# seqtk seq -A ../../nextflow/data/passenger/test_run/unique_with_at_least_two_reads/2B_S2_001.fastq > 2B_S2_001.fasta
-# makeblastdb -in 2B_S2_001.fasta -dbtype nucl -out 2B_S2_001.db
-# blastn -max_target_seqs 100000000 -query around-deleted-corrected.fasta -db 2B_S2_001.db -task "blastn-short" -outfmt 6 -evalue 1000 -word_size 7 -perc_identity 95 > 2B_S2_001.blast.tsv
-
-for file in ../../nextflow/data/passenger/test_run/unique_with_at_least_two_reads/*.fastq
+#for file in ../../nextflow/data/passenger/test_run/unique_with_at_least_two_reads/*.fastq
+for file in ../../nextflow/data/passenger/8A/AV_IgYKSTOP_8A_UniqueSequences_atleast-2.fastq
 do
     filename=$(basename "$file" .fastq)
     seqtk seq -A "$file" > "$filename.fasta"
