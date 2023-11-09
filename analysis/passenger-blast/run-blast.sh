d2cf2d824c47f2876a172df706cfcb6c3943e263
Erick Matsen <ematsen@gmail.com>
2023-11-09 10:17:31 -0800

    9a

--- analysis/passenger-blast/run-blast.sh
+++ analysis/passenger-blast/run-blast.sh
@@ -1,8 +1,11 @@
-#for file in ../../nextflow/data/passenger/test_run/unique_with_at_least_two_reads/*.fastq
-for file in ../../nextflow/data/passenger/8A/AV_IgYKSTOP_8A_UniqueSequences_atleast-2.fastq
+# for file in ../../nextflow/data/passenger/test_run/unique_with_at_least_two_reads/*.fastq
+# for file in ../../nextflow/data/passenger/8A/AV_IgYKSTOP_8A_UniqueSequences_atleast-2.fastq
+# query="around-deleted-corrected.fasta"
+query="around-deleted-9a.fasta"
+for file in ../../nextflow/data/passenger/9A/9A_collapse-unique_atleast-2.fastq
 do
     filename=$(basename "$file" .fastq)
     seqtk seq -A "$file" > "$filename.fasta"
     makeblastdb -in "$filename.fasta" -dbtype nucl -out "$filename.db"
-    blastn -max_target_seqs 100000000 -query around-deleted-corrected.fasta -db "$filename.db" -task "blastn-short" -outfmt 6 -evalue 1000 -word_size 7 -perc_identity 95 > "$filename.blast.tsv"
+    blastn -max_target_seqs 100000000 -query "$query" -db "$filename.db" -task "blastn-short" -outfmt 6 -evalue 1000 -word_size 7 -perc_identity 95 > "$filename.blast.tsv"
 done
