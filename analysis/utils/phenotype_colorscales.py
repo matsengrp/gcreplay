1fa01de15aaacb18f5e4d2eaa757c1027e71f6b4
Jared Galloway <jaredgalloway07@gmail.com>
2024-08-07 12:57:41 -0700

    Revert "added stacking code to NDS_LB notebook (#52)"
    This reverts commit ba402a4933e12c641ca37cc30f6a98410443c6cd.

--- analysis/utils/phenotype_colorscales.py
+++ analysis/utils/phenotype_colorscales.py
@@ -7,13 +7,11 @@ import numpy as np
 
 class affinity_dms:
     """Based on Tyler's R code for DMS heatmaps"""
-
     colors = ["#A94E35", "#A94E35", "#F48365", "#FFFFFF", "#7378B9", "#383C6C"]
     norm = mc.Normalize(vmin=-3.5, vmax=1)
     positions = np.array([0, 1, 2.25, 3.5, 4, 4.5]) / 4.5
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
+    cmap = mc.LinearSegmentedColormap.from_list("custom_cmap",
+                                                list(zip(positions, colors)))
     cmap.set_bad(color="#b3b3b3")
 
 
@@ -21,19 +19,8 @@ class affinity_trees:
     colors = ["#A94E35", "#F48365", "#FFFFFF", "#7378B9", "#383C6C"]
     norm = mc.Normalize(vmin=-3, vmax=3)
     positions = np.linspace(0, 1, len(colors))
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
-    cmap.set_bad(color="#b3b3b3")
-
-
-class affinity_trees_grey_middle:
-    colors = ["#A94E35", "#F48365", "#808080", "#7378B9", "#383C6C"]
-    norm = mc.Normalize(vmin=-3, vmax=3)
-    positions = np.linspace(0, 1, len(colors))
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
+    cmap = mc.LinearSegmentedColormap.from_list("custom_cmap",
+                                                list(zip(positions, colors)))
     cmap.set_bad(color="#b3b3b3")
 
 
@@ -41,9 +28,8 @@ class expression_dms:
     colors = ["#A94E35", "#F48365", "#FFFFFF", "#7378B9", "#383C6C"]
     norm = mc.TwoSlopeNorm(vcenter=0, vmin=-2.25, vmax=0.25)
     positions = np.linspace(0, 1, len(colors))
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
+    cmap = mc.LinearSegmentedColormap.from_list("custom_cmap",
+                                                list(zip(positions, colors)))
     cmap.set_bad(color="#b3b3b3")
 
 
@@ -51,9 +37,8 @@ class expression_trees:
     colors = ["#A94E35", "#F48365", "#FFFFFF", "#7378B9", "#383C6C"]
     norm = mc.TwoSlopeNorm(vcenter=0, vmin=-2.5, vmax=0.5)
     positions = np.linspace(0, 1, len(colors))
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
+    cmap = mc.LinearSegmentedColormap.from_list("custom_cmap",
+                                                list(zip(positions, colors)))
     cmap.set_bad(color="#b3b3b3")
 
 
@@ -61,9 +46,8 @@ class psr_dms:
     colors = list(reversed(["#A94E35", "#F48365", "#FFFFFF", "#7378B9", "#383C6C"]))
     norm = mc.TwoSlopeNorm(vcenter=0, vmin=-0.5, vmax=2.25)
     positions = np.linspace(0, 1, len(colors))
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
+    cmap = mc.LinearSegmentedColormap.from_list("custom_cmap",
+                                                list(zip(positions, colors)))
     cmap.set_bad(color="#b3b3b3")
 
 
@@ -71,9 +55,8 @@ class psr_trees:
     colors = list(reversed(["#A94E35", "#F48365", "#FFFFFF", "#7378B9", "#383C6C"]))
     norm = mc.TwoSlopeNorm(vcenter=0, vmin=-1, vmax=3)
     positions = np.linspace(0, 1, len(colors))
-    cmap = mc.LinearSegmentedColormap.from_list(
-        "custom_cmap", list(zip(positions, colors))
-    )
+    cmap = mc.LinearSegmentedColormap.from_list("custom_cmap",
+                                                list(zip(positions, colors)))
     cmap.set_bad(color="#b3b3b3")
 
 
@@ -92,72 +75,18 @@ def main():
 
     _, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
 
-    sns.histplot(
-        pd.read_csv(
-            dms, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())
-        ).delta_bind_CGG,
-        label="DMS",
-        ax=ax1,
-    )
-    sns.histplot(
-        pd.Series(
-            [
-                node.delta_bind
-                for tree in trees
-                for node in tree.tree.traverse()
-                if not np.isnan(node.delta_bind)
-            ]
-        ),
-        label="trees",
-        zorder=0,
-        ax=ax1,
-    )
+    sns.histplot(pd.read_csv(dms, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())).delta_bind_CGG, label="DMS", ax=ax1)
+    sns.histplot(pd.Series([node.delta_bind for tree in trees for node in tree.tree.traverse() if not np.isnan(node.delta_bind)]), label="trees", zorder=0, ax=ax1)
     ax1.legend()
 
-    sns.histplot(
-        pd.read_csv(
-            dms, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())
-        ).delta_expr,
-        label="DMS",
-        ax=ax2,
-    )
-    sns.histplot(
-        pd.Series(
-            [
-                node.delta_expr
-                for tree in trees
-                for node in tree.tree.traverse()
-                if not np.isnan(node.delta_expr)
-            ]
-        ),
-        label="trees",
-        zorder=0,
-        ax=ax2,
-    )
+    sns.histplot(pd.read_csv(dms, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())).delta_expr, label="DMS", ax=ax2)
+    sns.histplot(pd.Series([node.delta_expr for tree in trees for node in tree.tree.traverse() if not np.isnan(node.delta_expr)]), label="trees", zorder=0, ax=ax2)
     ax2.legend()
 
-    sns.histplot(
-        pd.read_csv(
-            dms, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())
-        ).delta_psr,
-        label="DMS",
-        ax=ax3,
-    )
-    sns.histplot(
-        pd.Series(
-            [
-                node.delta_psr
-                for tree in trees
-                for node in tree.tree.traverse()
-                if not np.isnan(node.delta_psr)
-            ]
-        ),
-        label="trees",
-        zorder=0,
-        ax=ax3,
-    )
+    sns.histplot(pd.read_csv(dms, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())).delta_psr, label="DMS", ax=ax3)
+    sns.histplot(pd.Series([node.delta_psr for tree in trees for node in tree.tree.traverse() if not np.isnan(node.delta_psr)]), label="trees", zorder=0, ax=ax3)
     ax3.legend()
-
+    
     plt.tight_layout()
     plt.show()
 
