bb667b12c806767a7c19701306b717fffd2a6426
Jared Galloway <jaredgalloway07@gmail.com>
2025-04-30 12:46:50 -0600

    159 rescaling and stacking beast slices (#160)
    * Rescaled - and tree root fix, with notebook for plotting

--- bin/beast2ete.py
+++ bin/beast2ete.py
@@ -1,73 +1,108 @@
 #!/usr/bin/env python
 
-from utils import *
-import beast_utils
+from utils import aa, mutations, naive_hk_bcr_nt
+from beast_utils import beast_trees_as_ete
+
+import pickle
 import argparse
-import ete3
-from historydag import beast_loader
 import pandas as pd
 import numpy as np
-from Bio.Seq import Seq
-import seaborn as sns
-import matplotlib.pyplot as plt
 import os
+import re
 from functools import partial
 
-parser = argparse.ArgumentParser(description='A script that takes in string and file arguments')
+# TODO remove
+# import ete3
+# from historydag import beast_loader
+# from Bio.Seq import Seq
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(
+    description="A script that takes in string and file arguments"
+)
+
 
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
 
 # Adding string arguments
-parser.add_argument('--xml_file', type=str, help='')
-parser.add_argument('--nexus_file', type=str, help='')
-parser.add_argument('--dms_df', type=str, help='')
-parser.add_argument('--pos_df', type=str, help='')
-parser.add_argument('--outdir', type=str, help='')
+parser.add_argument("--xml_file", type=str, help="")
+parser.add_argument("--nexus_file", type=str, help="")
+parser.add_argument("--dms_df", type=str, help="")
+parser.add_argument("--pos_df", type=str, help="")
+parser.add_argument("--outdir", type=str, help="")
 # parser.add_argument('--pkldir', type=str, required=False, default=None)
-parser.add_argument("--save_pkl_trees", type=str2bool, nargs='?',
-                        const=True, default=False,
-                        help="Activate nice mode.")
-parser.add_argument('--burn_frac', type=float, required=False, default=0.9, help='')
-parser.add_argument('--igk_idx', type=int, required=False, default=336, help='')
-parser.add_argument('--igh_frame', type=int, required=False, default=1, help='')
-parser.add_argument('--igk_frame', type=int, required=False, default=1, help='')
+parser.add_argument(
+    "--save_pkl_trees",
+    type=str2bool,
+    nargs="?",
+    const=True,
+    default=False,
+    help="Save all MCMC trees as pickles.",
+)
+parser.add_argument("--burn_frac", type=float, required=False, default=0.9, help="")
+parser.add_argument("--igk_idx", type=int, required=False, default=336, help="")
+parser.add_argument("--igh_frame", type=int, required=False, default=1, help="")
+parser.add_argument("--igk_frame", type=int, required=False, default=1, help="")
+# TODO add a parameter to save only a single pkl tree
+parser.add_argument(
+    "--save_single_pkl",
+    type=str2bool,
+    nargs="?",
+    const=True,
+    default=True,
+    help="Save the last MCMC tree as a pickle.",
+)
+parser.add_argument(
+    "--n_phenotype_slices",
+    type=int,
+    required=False,
+    default=100,
+    help="Number of phenotype slices to take between naive and sampling time. Default is 100.",
+)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if not os.path.exists(args.outdir): os.mkdir(args.outdir)
-    if args.save_pkl_trees:
-        if not os.path.exists(f"{args.outdir}/pkl_ete_trees"): os.mkdir(f"{args.outdir}/pkl_ete_trees")
-    naive_sequence=naive_hk_bcr_nt 
+    if not os.path.exists(args.outdir):
+        os.mkdir(args.outdir)
+    if args.save_pkl_trees or args.save_single_pkl:
+        if not os.path.exists(f"{args.outdir}/pkl_ete_trees"):
+            os.mkdir(f"{args.outdir}/pkl_ete_trees")
 
+    naive_sequence = naive_hk_bcr_nt
 
     # Gather and wrangle DMS data for phenotyping function
-    dms_df = pd.read_csv(args.dms_df, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype()))
+    dms_df = pd.read_csv(
+        args.dms_df, index_col="mutation", dtype=dict(position_IMGT=pd.Int16Dtype())
+    )
     # remove linker sites
     dms_df = dms_df[dms_df.chain != "link"]
     dms_df["WT"] = dms_df.wildtype == dms_df.mutant
     assert dms_df.position_IMGT.max() < 1000
     dms_df["site"] = [
-        f"{chain}-{str(pos).zfill(3)}" for chain, pos in zip(dms_df.chain, dms_df.position_IMGT)
+        f"{chain}-{str(pos).zfill(3)}"
+        for chain, pos in zip(dms_df.chain, dms_df.position_IMGT)
     ]
 
-    pos_df = pd.read_csv(args.pos_df, dtype=dict(site=pd.Int16Dtype()), index_col="site_scFv")
+    pos_df = pd.read_csv(
+        args.pos_df, dtype=dict(site=pd.Int16Dtype()), index_col="site_scFv"
+    )
 
     # position maps for heavy and light chain that can be used in gctree render
-    igh_pos_map = pos_df.loc[pos_df.chain
-                             == "H", "site"].reset_index(drop=True)
-    igk_pos_map = pos_df.loc[pos_df.chain
-                             == "L", "site"].reset_index(drop=True)
+    igh_pos_map = pos_df.loc[pos_df.chain == "H", "site"].reset_index(drop=True)
+    igk_pos_map = pos_df.loc[pos_df.chain == "L", "site"].reset_index(drop=True)
 
     igk_idx, igh_frame, igk_frame = args.igk_idx, args.igh_frame, args.igk_frame
 
@@ -75,12 +110,7 @@ if __name__ == '__main__':
     naive_igk_aa = aa(naive_hk_bcr_nt[igk_idx:], igk_frame)
 
     def phenotypes(
-        igk_idx,
-        naive_igh_aa,
-        naive_igk_aa,
-        igh_pos_map,
-        igk_pos_map,
-        sequence
+        igk_idx, naive_igh_aa, naive_igk_aa, igh_pos_map, igk_pos_map, sequence
     ):
         igh_aa = aa(sequence[:igk_idx], igh_frame)
         igk_aa = aa(sequence[igk_idx:], igk_frame)
@@ -93,73 +123,63 @@ if __name__ == '__main__':
             return pd.Series([dms_df[field].min() for field in fields], index=fields)
         return dms_df.loc[all_mutations, fields].sum(0)
 
-
-    def phenotype_slice(tree, age):
+    def phenotype_slice(tree, time):
         def is_leaf_fn(node):
-            return node.age <= age and (node.up is None or node.up.age > age)
+            return node.time >= time and (node.up is None or node.up.time < time)
+
+        if time < 0.0 or time > 1.0:
+            raise ValueError(f"time {time} is outside tree time range 0, 1")
 
         slice_population = []
         for node in tree.iter_leaves(is_leaf_fn=is_leaf_fn):
-            if node.age == age:
+            if node.time == time:
                 slice_population.append(node.phenotypes)
             else:
                 current_phenotypes = node.up.phenotypes
-                for mut_event in node.mutations:
 
-                    if mut_event['age'] < age:
+                for mut_event in node.mutations:
+                    if mut_event["time"] < time:
+                        current_phenotypes = mut_event["derived_phenotypes"]
+                    else:
                         break
-                    current_phenotypes = mut_event['phenotypes']
 
                 slice_population.append(current_phenotypes)
 
         result = pd.concat(slice_population, axis=1).T
         return result
 
-    tree_start_idx, ete_trees = beast_utils.beast_trees_as_ete(
-        args.xml_file, 
-        args.nexus_file, 
+    tree_start_idx, ete_trees = beast_trees_as_ete(
+        args.xml_file,
+        args.nexus_file,
         naive_sequence,
-        partial(phenotypes, igk_idx, naive_igh_aa, naive_igk_aa, igh_pos_map, igk_pos_map),
-        burn_frac=args.burn_frac
+        partial(
+            phenotypes, igk_idx, naive_igh_aa, naive_igk_aa, igh_pos_map, igk_pos_map
+        ),
+        burn_frac=args.burn_frac,
     )
 
     slices = []
     nodes = []
+
     for tree_idx, ete_tree in enumerate(ete_trees, tree_start_idx):
         if args.save_pkl_trees:
-            pickle.dump(ete_tree, open(f"{args.outdir}/pkl_ete_trees/{tree_idx}.pkl", "wb"))
+            pickle.dump(
+                ete_tree, open(f"{args.outdir}/pkl_ete_trees/{tree_idx}.pkl", "wb")
+            )
+        if args.save_single_pkl and tree_idx == tree_start_idx + len(ete_trees) - 1:
+            pickle.dump(
+                ete_tree, open(f"{args.outdir}/last_sampled_tree.pkl", "wb")
+            )
 
-        # phenotype slices through tree time
-        for age in np.linspace(0, 20, 20):
-            slice = phenotype_slice(ete_tree, age)
-            slice_summary = slice.mean()
-            slice_summary["age"] = age
-            slice_summary["lineages"] = len(slice)
-            slice_summary["tree_idx"] = tree_idx
-            slices.append(slice_summary)
+        for time in np.linspace(0, 1, args.n_phenotype_slices):
+            slice = phenotype_slice(ete_tree, time)
 
-        # aggregated node data
-        for node in ete_tree.traverse():
-            if node.mutations:
-                for mut_event in node.mutations:
-                    nodes.append(
-                        [
-                            tree_idx, 
-                            mut_event["age"], 
-                            mut_event["phenotypes"].delta_bind_CGG, 
-                            mut_event["phenotypes"].delta_expr
-                        ]
-                    )
+            slice["time"] = time.round(3)
+            slice["tree_idx"] = tree_idx
+            assert np.all(slice.index.values == range(len(slice)))
+            slice.index.name = "entry_idx"
+            slice.reset_index(drop=False, inplace=True)
+            slices.append(slice)
 
-            nodes.append([tree_idx, node.age, node.phenotypes.delta_bind_CGG, node.phenotypes.delta_expr])
-
-    slice_df = pd.concat(slices, axis=1).T
-    slice_df["time"] = slice_df.age.max() - slice_df.age
-    slice_df["gc"] = "-".join(args.outdir.split("-")[1:])
+    slice_df = pd.concat(slices).reset_index(drop=True)
     slice_df.to_csv(f"{args.outdir}/slice_df.csv", index=False)
-
-    node_df = pd.DataFrame(nodes, columns=["tree_idx", "age", "delta_bind_CGG", "delta_expr"])
-    node_df["time (days post-immunization)"] = pd.cut(
-            node_df.age.max() - node_df.age, bins=100
-    ).apply(lambda x: 0 if x.left < 0 else x.right)
-    node_df.to_csv(f"{args.outdir}/node_df.csv")
--- bin/beast_utils.py
+++ bin/beast_utils.py
@@ -4,8 +4,6 @@ from functools import partial
 import numpy as np
 
 
-
-
 def beast_trees_as_ete(
     beast_xml_path,
     beast_nexus_path,
@@ -36,11 +34,9 @@ def dendropy_beast_trees_to_ete(beast_trees, naive_label, phenotype_fn):
     documentation for beast_trees_as_ete.
     """
     ete_iterator = map(
-        partial(beast_dendropy_to_ete, phenotype_fn=phenotype_fn), 
-        beast_trees
+        partial(beast_dendropy_to_ete, phenotype_fn=phenotype_fn), beast_trees
     )
-    bad_root = lambda tree: not tree.get_leaves_by_name(naive_label)[0].up.is_root()
-    return [tree for tree in ete_iterator if not bad_root(tree)]
+    return [tree for tree in ete_iterator]
 
 
 def dendropy_trees_from_beast(
@@ -58,27 +54,9 @@ def dendropy_trees_from_beast(
     if skip_first_tree:
         next(tree_iterator)
     beast_trees = list(tree_iterator)
-    tree_start_idx = int(len(beast_trees)*burn_frac)
+    tree_start_idx = int(len(beast_trees) * burn_frac)
     any((set_dendropy_tree_node_ages(tree) for tree in beast_trees[tree_start_idx:]))
     return tree_start_idx, beast_trees[tree_start_idx:]
-
-
-#def set_dendropy_tree_node_ages(tree):
-#    """
-#    Set the age and root_distance attributes (in place) of nodes in tree. The
-#    age of a node is taken to be longest path from the root to any leaf minus
-#    the length of the path from the root to the given node. The age of non-naive
-#    leaf nodes may not be exactly 0.0 due to floating point arithmetic.
-#    """
-#    last_time = max(tree.calc_node_root_distances())
-#    def age_function(node): 
-#        age = last_time - node.root_distance
-#        if node.is_leaf():
-#            age = round(age, 6)
-#            assert age < node.up.age
-#        return age
-#    tree.calc_node_ages(set_node_age_fn=age_function)
-#    return None
 
 
 def set_dendropy_tree_node_ages(tree):
@@ -96,7 +74,6 @@ def set_dendropy_tree_node_ages(tree):
         node.age = round(node.age)
 
     return None
-
 
 
 def format_history_all(history_all_string):
@@ -117,6 +94,8 @@ def beast_dendropy_to_ete(tree, phenotype_fn):
     attributes, based on the dendropy tree.
     """
 
+    # assert False
+
     assert tree.is_rooted
     ete_tree = ete3.Tree(dist=0.0)
     node_pairing = zip(
@@ -132,27 +111,114 @@ def beast_dendropy_to_ete(tree, phenotype_fn):
             node_label = child.taxon.label if child.taxon else None
             ete_child = ete3.Tree(name=node_label, dist=child.edge_length)
             ete_node.add_child(ete_child)
+
+        # assert False
         if len(dendropy_node.comments) == 2:
+            # assert False, f"comments[1]: \n {dendropy_node.comments[1]}"
             mutation_history = format_history_all(dendropy_node.comments[1])
             mutation_history.sort(key=lambda x: x[1], reverse=True)
             parent_sequence = list(ete_node.up.sequence)
 
-            previous_event_age = ete_node.up.age
+            # previous_event_age = ete_node.up.age
             for site, age, ancestral, derived in mutation_history:
                 site -= 1
-                time_to_event = previous_event_age - age
+                # time_to_event = previous_event_age - age
+                time_since_parent = ete_node.up.age - age
+                time_until_child = age - ete_node.age
+
+                ancestral_phenotypes = phenotype_fn("".join(parent_sequence))
+
                 parent_sequence[site] = derived
 
                 event = dict(
-                        site=site,
-                        ancestral=ancestral,
-                        derived=derived,
-                        age=age,
-                        time_to_event=time_to_event,
-                        phenotypes=phenotype_fn("".join(parent_sequence))
+                    site=site,
+                    ancestral=ancestral,
+                    derived=derived,
+                    # age=age,
+                    time_since_parent=time_since_parent,
+                    time_until_child=time_until_child,
+                    derived_phenotypes=phenotype_fn("".join(parent_sequence)),
+                    ancestral_phenotypes=ancestral_phenotypes,
                 )
 
                 ete_node.mutations.append(event)
-                previous_event_age = age
+                # previous_event_age = age
+
+    # now, we want to fix the tree such that naive is the root
+    outgroup = ete_tree & "naive@0"
+
+    # make sure the psuedo root has no mutations
+    assert not ete_tree.mutations
+
+    # re-root the tree such that naive is new root.
+    ete_tree.remove_child(outgroup)
+    ete_tree.dist = outgroup.dist
+    outgroup.dist = 0
+    outgroup.add_child(ete_tree)
+    ete_tree = outgroup
+
+    # now, we need to correctly add mutations that were from pseudo root to naive
+    # as mutations from naive to pseudo root
+    psuedo_root = ete_tree.get_children()[0]
+    if ete_tree.mutations:
+        for mut_event in reversed(ete_tree.mutations):
+            reversion_event = dict(
+                site=mut_event["site"],
+                ancestral=mut_event["derived"],
+                derived=mut_event["ancestral"],
+                time_since_parent=mut_event["time_until_child"],
+                time_until_child=mut_event["time_since_parent"],
+                derived_phenotypes=mut_event["ancestral_phenotypes"],
+                ancestral_phenotypes=mut_event["derived_phenotypes"],
+            )
+
+            psuedo_root.mutations.append(reversion_event)
+
+    # now remove the mutations from the new naive root.
+    ete_tree.mutations = []
+
+    # rescale times to be between 0 and 1
+    for node in ete_tree.traverse():
+        node.time = node.get_distance(ete_tree)
+
+    leaf_ages = [leaf.time for leaf in ete_tree]
+    assert np.allclose(leaf_ages[0], leaf_ages)
+    time_scale_factor = leaf_ages[0]
+    for node in ete_tree.traverse(strategy="preorder"):
+        node.time /= time_scale_factor
+        node.dist /= time_scale_factor
+
+        del node.age
+
+        if node.mutations:
+            for mut_event in node.mutations:
+                mut_event["time_since_parent"] /= time_scale_factor
+                mut_event["time_until_child"] /= time_scale_factor
+                mut_event["time"] = node.up.time + mut_event["time_since_parent"]
+                assert np.isclose(
+                    mut_event["time"], node.time - mut_event["time_until_child"]
+                )
+
+                # we no longer need these after correcting root mutations
+                del mut_event["time_since_parent"]
+                del mut_event["time_until_child"]
+
+                assert mut_event["time"] > node.up.time
+                assert mut_event["time"] < node.time
+
+    # some final checks on the tree
+    assert len(ete_tree.children) == 1
+    assert ete_tree.time == 0, f"naive time = {ete_tree.time}"
+    for node in ete_tree.traverse():
+        assert np.isclose(
+            node.get_distance(ete_tree), node.time
+        ), f"node {node.name} has distance {node.get_distance(ete_tree)} != time {node.time}"
+        if node.is_leaf():
+            assert np.isclose(node.time, 1), f"leaf time should be 1, {node.time}"
+
+        for i in range(len(node.mutations) - 1):
+            assert (
+                node.mutations[i]["time"] < node.mutations[i + 1]["time"]
+            ), f"mutation {i} time {node.mutations[i]['time']} should be less than mutation {i+1} time {node.mutations[i+1]['time']}"
 
     return ete_tree
